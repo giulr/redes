ae55ac89cecbd542d059a25758aa15aa7fd172a6
Giuliard Rodrigues <giuliard.r@gmail.com>
2016-12-06 18:56:53 -0300

    conexao_sockets

--- servidorTrabalho.py
+++ servidorTrabalho.py
@@ -1,93 +1,62 @@
 from socket import * # sockets
 from threading import Thread # thread
-import time
-
-#conn e coneccao com socet
-
-#tratamento do conn.recv()
-def receberCliente(conn):
-    try:
-        dado = conn.recv(1024)
-    except Exception as all:
-        print "Erro: S:12 Desconectar cliente!"
-        dado = '//stop()'
-        conn.close()
-    return dado
-
-#tratamento de conn.send()
-def mandarCliente(conn, mensagem):
-    try:
-        conn.send(mensagem)
-    except Exception as e:
-        print "Erro: S:22 Cliente nao existe!"
-
-def removeConnAddr(addr):
-    posicao = posaddr(addr)
-    del connList[posicao]
-    del addrList[posicao]
-    del nickList[posicao]
-
-def addConnAddr(conn,ddr,name):
-    connList[len(connList):] = [conn]
-    addrList[len(addrList):] = [addr]
-    nickList[len(nickList):] = [name]
-
-#retornar posicao do endereco
-def posaddr(addr):
-    if addr in addrList:
-        return addrList.index(addr)
-    else:
-        print "Erro S:39 Endereco nao existe!"
-
-#Mandar para todos.
-def mandarParaTodos(mensagem):
-    for i in connList:
-        mandarCliente(i,mensagem)
 
 def reDadoCliente(conn,addr):
-    dado = receberCliente(conn)
-    nickList[posaddr(addr)] = dado
-    nickname = dado
-    sentence = "Ola " + nickname +"\n" + nickname + " >>"
-    mandarCliente(conn, sentence)
-    print nickname + " entrou na sala..."
-    #mandarParaTodos(m)
+    dado = conn.recv(1024);
+    nickname = dado;
+    sentence = "Ola " + nickname +"!";
+    conn.send(sentence);
+    print nickname + " entrou na sala...";
     while 1:
-        dado = receberCliente(conn)
-        if dado == "//stop()":
-            print"%s >> Saiu!" % nickList[posaddr(addr)]
-            removeConnAddr(addr)
-            break
-        elif dado == "//nick()":
-            mandarCliente(conn,"Digite seu novo nick: ")
-            nickname = receberCliente(conn)
-            sentence = "Seu novo nickname e: " + nickname
-            mandarCliente(conn, sentence)
-        else:
-            msgServer = dado + " \n" + nickname + ">>"
-            mandarCliente(conn, msgServer)
-            print nickname + " disse: " + dado
-
+        try:
+            dado = conn.recv(1024);
+            if dado == "//stop" or dado == '':
+                print"Cliente [%s:%s]:Saiu" % (addr[0],addr[1])
+                sentence = "Ate a proxima";
+                conn.send(sentence)
+                conn.close()
+                break
+            if dado == "altNick":
+                conn.send("Digite seu novo nick: ");
+                nickname = conn.recv(1024);
+                sentence = "Seu novo nickname e: " + nickname;
+                conn.send(sentence);
+            else:
+                msgServer = nickname + " disse: " + dado;
+                enviarMsgTodos(serverSocket,msgServer);
+                print nickname + " disse: " + dado;
+        except:
+            enviarMsgTodos(serverSocket, "Cliente offline")
+def enviarMsgTodos(serverSocket, mensagem):
+    for socket in sockets:
+        if socket != serverSocket :
+            try :
+                socket.send(mensagem)
+            except :
+                socket.close()
+                print "erro"
+                if socket in sockets:
+                    sockets.remove(socket)
 
 #variaveis globais
 serverName = 'localhost' # ip do servidor (em branco)
 serverPort = 12000 # porta a se conectar
 serverSocket = socket(AF_INET,SOCK_STREAM) # criacao do socket TCP
 serverSocket.bind((serverName,serverPort)) # bind do ip do servidor com a porta
-serverSocket.listen(1) # socket pronto para "ouvir" conexoes
-connList = [] #lista de coneccoes
-addrList = [] #lista de enderecos
-nickList = [] #lista de nomes
+serverSocket.listen(10) # socket pronto para "ouvir" conexoes
+sockets = [];
+addresses = [];
 while 1:
     print "Servidor TCP esperando conexoes na porta %d ..." % (serverPort)
     connectionSocket, addr = serverSocket.accept() # aceita as conexoes dos clientes
-    addConnAddr(connectionSocket,addr,"SemNome")
-
-    print"Cliente [%s:%s]:Conectado!\nLista de nomes:" % (addr[0], addr[1])
-    print nickList
+    print"Cliente [%s:%s]:Conectado!" % (addr[0], addr[1])
     t = Thread(target = reDadoCliente, args = (connectionSocket,addr))
     t.start()
-
+    sockets.append(connectionSocket)
+    
+    
+    
+    
     #sentence = connectionSocket.recv(1024) # recebe dados do cliente
     #capitalizedSentence = sentence.upper() # converte em letras maiusculas
     #print "Cliente %s enviou: %s, transformando em: %s" % (addr, sentence, capitalizedSentence)
